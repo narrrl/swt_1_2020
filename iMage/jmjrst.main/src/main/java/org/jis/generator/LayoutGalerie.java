e6012bbd61b519b84e5a34c46b8685c4127b06a7
Nils Pukropp <nils.pukropp@outlook.de>
2020-04-23 22:31:33 +0200

    removed unused variable j

--- iMage/jmjrst.main/src/main/java/org/jis/generator/LayoutGalerie.java
+++ iMage/jmjrst.main/src/main/java/org/jis/generator/LayoutGalerie.java
@@ -168,8 +168,6 @@ public class LayoutGalerie {
 	        {
 	          e.printStackTrace();
 	        }
-	
-	        int j = 0;
 	        int count = 0;
 	        int page = 0;
 	        String index = "index." + layout.getPrefix();
@@ -201,8 +199,6 @@ public class LayoutGalerie {
 	          {
 	            System.out.print(m.mes.getString("Generator.10") + images[i].getName() + "\t . . . ");
 	          }
-	
-	          j++;
 	          count++;
 	
 	          // generate small image
