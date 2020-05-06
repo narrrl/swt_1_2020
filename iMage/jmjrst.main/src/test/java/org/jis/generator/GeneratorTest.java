a37311b9120924b27258c81ceb0d564ff81c45fd
Nils Pukropp <nils.pukropp@outlook.de>
2020-05-06 04:42:52 +0200

    created tests

--- iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
+++ iMage/jmjrst.main/src/test/java/org/jis/generator/GeneratorTest.java
@@ -3,6 +3,7 @@ package org.jis.generator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import javax.imageio.*;
@@ -12,13 +13,18 @@ import javax.imageio.stream.ImageInputStream;
 import javax.imageio.stream.ImageOutputStream;
 import java.awt.image.BufferedImage;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
+import java.nio.file.Files;
 import java.text.MessageFormat;
 import java.text.SimpleDateFormat;
+import java.util.Arrays;
 import java.util.Date;
 import java.util.Locale;
+import java.util.Vector;
 
 import static org.junit.Assert.*;
 
@@ -67,10 +73,10 @@ public class GeneratorTest {
     this.testImage = null;
     this.imeta = null;
     this.rotatedImageTestResult = null;
-    
+
     final URL imageResource = this.getClass().getResource(IMAGE_FILE);
     imageName = extractFileNameWithoutExtension(new File(imageResource.getFile()));
-   
+
     try (ImageInputStream iis = ImageIO.createImageInputStream(imageResource.openStream())) {
       ImageReader reader = ImageIO.getImageReadersByFormatName("jpg").next();
       reader.setInput(iis, true);
@@ -102,12 +108,11 @@ public class GeneratorTest {
     SimpleDateFormat sdf = new SimpleDateFormat("MM-dd_HH.mm.ss.SSS");
     String time = sdf.format(new Date());
 
-    File outputFile = new File(
-        MessageFormat.format("{0}/{1}_rotated_{2}.jpg", TEST_DIR, imageName, time));
+    File outputFile = new File(MessageFormat.format("{0}/{1}_rotated_{2}.jpg", TEST_DIR, imageName, time));
 
     if (this.rotatedImageTestResult != null) {
       try (FileOutputStream fos = new FileOutputStream(outputFile);
-           ImageOutputStream ios = ImageIO.createImageOutputStream(fos)) {
+          ImageOutputStream ios = ImageIO.createImageOutputStream(fos)) {
         ImageWriter writer = ImageIO.getImageWritersByFormatName("jpg").next();
         writer.setOutput(ios);
 
@@ -200,12 +205,80 @@ public class GeneratorTest {
   }
 
   /**
+   * Test for
+   * {@link org.jis.generator.Generator#generateImage(File, File, boolean, int, int, String)}.
+   * Copies a file with two times its height and width.
+   */
+  @Test
+  public void testGenerateImage() throws IOException, URISyntaxException {
+    final File resourceFolder = new File(this.getClass().getResource(File.separator).toURI());
+    File image = new File(resourceFolder, "imageCopy.jpg");
+    image.createNewFile();
+    ImageIO.write(this.testImage, "jpg", image);
+    File imageDest = new File(resourceFolder, "imageResized.jpg");
+    imageDest.createNewFile();
+    this.generator.generateImage(image, imageDest, false, this.testImage.getWidth() * 2, this.testImage.getHeight() * 2,
+        "");
+    assertEquals(this.testImage.getHeight() * 2, ImageIO.read(new FileInputStream(imageDest)).getHeight());
+
+    assertEquals(this.testImage.getWidth() * 2, ImageIO.read(new FileInputStream(imageDest)).getWidth());
+    imageDest.delete();
+    image.delete();
+  }
+
+  /**
+   * Test for {@link org.jis.generator.Generator#rotate(File, int)}.
+   * 
+   * !!! {@link org.jis.generator.Generator#rotateImage(BufferedImage, double)} is
+   * used in this method and fails every time. Thats because it wants the angle as
+   * radian, which is not the case in rotate(file, int). As result rotate(file,
+   * int) always fails.
+   */
+  @Test
+  @Ignore
+  public void testRotate() throws IOException, URISyntaxException {
+    final File resourceFolder = new File(this.getClass().getResource(File.separator).toURI());
+    File image = new File(resourceFolder, "imageCopy.jpg");
+    image.createNewFile();
+    ImageIO.write(this.testImage, "jpg", image);
+    this.generator.rotate(image, 90);
+    assertEquals(ImageIO.read(image).getHeight(), this.testImage.getWidth());
+    assertEquals(ImageIO.read(image).getWidth(), this.testImage.getHeight());
+    image.delete();
+  }
+
+  /**
+   * Tests {@link org.jis.generator.Generator#createZip(File, Vector)}
+   */
+  @Test
+  public void testGenerate() throws IOException, URISyntaxException {
+    final File resourceFolder = new File(this.getClass().getResource(File.separator).toURI());
+    File image = new File(resourceFolder, "imageCopy.jpg");
+    image.createNewFile();
+    ImageIO.write(this.testImage, "jpg", image);
+    File dest = new File(resourceFolder, "imageResized.zip");
+    dest.createNewFile();
+    Vector<File> v = new Vector<>();
+    v.add(image);
+    this.generator.createZip(dest, v);
+    byte[] destBytes = Files.readAllBytes(dest.toPath());
+    dest.delete();
+    image.delete();
+    image.createNewFile();
+    ImageIO.write(this.testImage, "jpg", image);
+    dest.createNewFile();
+    this.generator.createZip(dest, v);
+    byte[] destBytes2 = Files.readAllBytes(dest.toPath());
+    assertTrue(Arrays.equals(destBytes2, destBytes));
+    dest.delete();
+    image.delete();
+  }
+
+  /**
    * Check if two images are identical - pixel wise.
    * 
-   * @param expected
-   *          the expected image
-   * @param actual
-   *          the actual image
+   * @param expected the expected image
+   * @param actual   the actual image
    * @return true if images are equal, false otherwise.
    */
   protected static boolean imageEquals(BufferedImage expected, BufferedImage actual) {
