9863155ae76f538957b642df043339f825506d51
Nils Pukropp <nils.pukropp@outlook.de>
2020-06-20 16:28:47 +0200

    created listeners

---
+++ iMage/iMage.iTiler/src/main/java/org/iMage/tiler/listeners/SaveAction.java
@@ -0,0 +1,44 @@
+package org.iMage.tiler.listeners;
+
+import org.iMage.tiler.Tiler;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+import java.io.IOException;
+
+public class SaveAction implements ActionListener {
+    final Tiler tiler;
+
+    public SaveAction(final Tiler tiler) {
+        super();
+        this.tiler = tiler;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        if (!tiler.hasMosaique()) {
+            JOptionPane.showMessageDialog(tiler,
+                    "Create a mosaique first!", "Info", JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+        JFileChooser chooser = new JFileChooser();
+        chooser.setDialogTitle("Save mosaique as ...");
+        FileFilter imageFilter = new FileNameExtensionFilter(
+                ".png", "png", "jpg");
+        chooser.setFileFilter(imageFilter);
+        int userSelection = chooser.showSaveDialog(tiler);
+        if (userSelection == JFileChooser.APPROVE_OPTION) {
+            File fileToSave = chooser.getSelectedFile();
+            try {
+                ImageIO.write(tiler.getImg(), "png", fileToSave);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
---
+++ iMage/iMage.iTiler/src/main/java/org/iMage/tiler/listeners/SelectAction.java
@@ -0,0 +1,33 @@
+package org.iMage.tiler.listeners;
+
+import org.iMage.tiler.Tiler;
+
+import javax.imageio.ImageIO;
+import javax.swing.*;
+import javax.swing.filechooser.FileFilter;
+import javax.swing.filechooser.FileNameExtensionFilter;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.File;
+
+public class SelectAction implements ActionListener {
+    final Tiler tiler;
+
+    public SelectAction(final Tiler tiler) {
+        super();
+        this.tiler = tiler;
+    }
+
+    @Override
+    public void actionPerformed(ActionEvent actionEvent) {
+        JFileChooser chooser = new JFileChooser();
+        FileFilter imageFilter = new FileNameExtensionFilter(
+                "only image files", ImageIO.getReaderFileSuffixes());
+        chooser.setFileFilter(imageFilter);
+        chooser.setAcceptAllFileFilterUsed(false);
+        chooser.setMultiSelectionEnabled(false);
+        chooser.showDialog(null, "Gimme that juicy anime tiddies");
+        File file = chooser.getSelectedFile();
+        tiler.setImage(file);
+    }
+}
