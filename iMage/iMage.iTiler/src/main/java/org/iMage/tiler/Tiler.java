4cd9e46fe41f4895c4efc5187b4443410e514129
Nils Pukropp <nils.pukropp@outlook.de>
2020-06-21 05:37:10 +0200

    created methods to run mosaique

--- iMage/iMage.iTiler/src/main/java/org/iMage/tiler/Tiler.java
+++ iMage/iMage.iTiler/src/main/java/org/iMage/tiler/Tiler.java
@@ -1,80 +1,87 @@
 package org.iMage.tiler;
 
+import org.iMage.mosaique.MosaiqueEasel;
 import org.iMage.mosaique.base.BufferedArtImage;
+import org.iMage.mosaique.rectangle.RectangleArtist;
+import org.iMage.mosaique.rectangle.RectangleShape;
+import org.iMage.tiler.panels.ArtisticPanel;
+import org.iMage.tiler.panels.ConfigurationPanel;
+import org.iMage.tiler.panels.ImagePanel;
 
 import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 import java.net.URL;
-import java.nio.Buffer;
-import java.util.Scanner;
+import java.util.ArrayList;
 
 import javax.imageio.ImageIO;
 import javax.swing.*;
-import javax.swing.border.Border;
+import javax.swing.border.TitledBorder;
 
 public class Tiler extends JFrame {
     private static final Dimension DIM = new Dimension(800, 450);
     private final Container pane;
-    private final BorderLayout layout;
     private final ImagePanel preview;
     private BufferedArtImage selectedImage;
-    private boolean imageSelected;
-    private boolean hasMosaique;
+    private BufferedArtImage mosaique;
+    private ArrayList<BufferedArtImage> tiles;
+    private final ArtisticPanel artisticPanel;
+    private final ConfigurationPanel configurationPanel;
 
     /**
      *
      */
     private static final long serialVersionUID = 1337L;
 
-    Tiler() throws IOException {
-        imageSelected = false;
+    Tiler() {
+        preview = new ImagePanel();
+        artisticPanel = new ArtisticPanel(this);
+        configurationPanel = new ConfigurationPanel(this);
         selectedImage = null;
+        pane = getContentPane();
+        BorderLayout layout = new BorderLayout();
         setTitle("iTiler");
         setSize(DIM);
         setPreferredSize(DIM);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setLocationRelativeTo(null);
         setResizable(false);
+
         URL url = ClassLoader.getSystemResource("icons/iTiler-icon.png");
         setIconImage(new ImageIcon(url).getImage());
-        pane = getContentPane();
-        layout = new BorderLayout();
+
         pane.setLayout(layout);
         pane.setBackground(new Color(30, 30, 30));
-        preview = new ImagePanel();
+
         pane.add(preview, BorderLayout.NORTH);
+
         createTextBox();
-        url = ClassLoader.getSystemResource("images/image2.png");
-        BufferedArtImage img = new BufferedArtImage(ImageIO.read(url));
-        preview.setLeftImage(img);
-        url = ClassLoader.getSystemResource("images/image1.png");
-        img = new BufferedArtImage(ImageIO.read(url));
-        preview.setRightImage(img);
+
         pack();
     }
 
-
     private void createTextBox() {
        JPanel box = new JPanel();
+
        box.setLayout(new BorderLayout());
        box.setPreferredSize(new Dimension(800, 160));
        box.setBackground(new Color(30, 30, 30));
-       box.add(new ConfigurationPanel(this), BorderLayout.NORTH);
-       box.add(new ArtisticPanel(), BorderLayout.SOUTH);
+
+       box.add(configurationPanel, BorderLayout.NORTH);
+       box.add(artisticPanel, BorderLayout.SOUTH);
+
+       box.setBorder(BorderFactory.createTitledBorder(
+               BorderFactory.createLineBorder(Color.LIGHT_GRAY), "Configuration",
+               TitledBorder.DEFAULT_JUSTIFICATION, TitledBorder.DEFAULT_POSITION, null, Color.LIGHT_GRAY));
+
        pane.add(box, BorderLayout.SOUTH);
     }
 
     public static void main(String[] args) {
         EventQueue.invokeLater(() -> {
             Tiler tiler;
-            try {
-                tiler = new Tiler();
-            } catch (IOException e) {
-                e.printStackTrace();
-                return;
-            }
+            tiler = new Tiler();
             tiler.setVisible(true);
         });
     }
@@ -87,7 +94,6 @@ public class Tiler extends JFrame {
             BufferedArtImage tmp = new BufferedArtImage(ImageIO.read(file));
             preview.setLeftImage(tmp);
             selectedImage = tmp;
-            imageSelected = true;
         } catch (IOException e) {
             JOptionPane.showMessageDialog(this,
                     "Couldn't find image!", "Info", JOptionPane.INFORMATION_MESSAGE);
@@ -95,14 +101,159 @@ public class Tiler extends JFrame {
     }
 
     public boolean hasMosaique() {
-        return hasMosaique;
+        return mosaique != null;
+    }
+
+    public void createMosaique() {
+        if (isImageSelected() && tiles != null) {
+            MosaiqueEasel easel = new MosaiqueEasel();
+            RectangleArtist artist;
+            int h;
+            int w;
+            try {
+                h = artisticPanel.getH() > 0 ? artisticPanel.getH() : 42;
+                w = artisticPanel.getW() > 0 ? artisticPanel.getW() : 42;
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(this, "Invalid width/height",
+                        "Info", JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+            artist = new RectangleArtist(tiles, w, h);
+            mosaique = new BufferedArtImage(easel.createMosaique(selectedImage.toBufferedImage(), artist));
+            preview.setRightImage(mosaique);
+            configurationPanel.setButtonEnabled(true);
+            return;
+        }
+        if (!isImageSelected() && tiles == null) {
+            JOptionPane.showMessageDialog(this, "Select a image and load tiles first!",
+                    "Info", JOptionPane.INFORMATION_MESSAGE);
+        } else if (!isImageSelected()) {
+            JOptionPane.showMessageDialog(this, "Select a image first!", "Info",
+                    JOptionPane.INFORMATION_MESSAGE);
+        } else {
+            JOptionPane.showMessageDialog(this, "Load tiles first!", "Info",
+                    JOptionPane.INFORMATION_MESSAGE);
+        }
     }
 
     public boolean isImageSelected() {
-        return imageSelected;
+        return selectedImage != null;
     }
 
     public BufferedImage getImg() {
-        return this.selectedImage.toBufferedImage();
+        return this.mosaique.toBufferedImage();
+    }
+
+    public void loadTiles(final File f) {
+        tiles = new ArrayList<>();
+
+        if (f != null && f.isDirectory()) {
+            File[] files = f.listFiles(file -> {
+                if (file == null) {
+                    return false;
+                }
+                return isImage(file);
+            });
+            if (files == null) {
+                JOptionPane.showMessageDialog(this, "No images found!", "Info",
+                        JOptionPane.INFORMATION_MESSAGE);
+                return;
+            }
+            JProgressBar bar = new JProgressBar(0, files.length);
+            bar.setValue(0);
+            bar.setStringPainted(true);
+            bar.setBackground(new Color(30, 30, 30));
+            JDialog dialog = new JDialog(this, "Progress");
+            dialog.add(bar);
+            dialog.pack();
+            dialog.setVisible(true);
+            dialog.setLocationRelativeTo(null);
+            Thread t = new Thread(() -> {
+                for (File tmp : files) {
+                    if (isImage(tmp)) {
+                        try {
+                            tiles.add(new BufferedArtImage(ImageIO.read(tmp)));
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                    bar.setValue(bar.getValue() + 1);
+                    if (bar.getValue() == files.length) {
+                        dialog.dispose();
+                    }
+                }
+            });
+            t.start();
+
+        }
+    }
+
+    private boolean isImage(final File f) {
+        for (String type : ImageIO.getReaderFileSuffixes()) {
+            if (f.getName().endsWith(type)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public void showTiles() {
+        if (tiles == null) {
+            JOptionPane.showMessageDialog(this, "Load tiles first!", "Info",
+                    JOptionPane.INFORMATION_MESSAGE);
+            return;
+        }
+        JDialog dialog = new JDialog(this, "Tiles");
+        JScrollPane scrollPane = new JScrollPane();
+        GridBagLayout l = new GridBagLayout();
+        JPanel p = new JPanel(l);
+        GridBagConstraints c = new GridBagConstraints();
+
+        dialog.setSize(new Dimension(530, 530));
+        dialog.setLayout(new BorderLayout(0, 0));
+        dialog.setResizable(false);
+        dialog.setLocationRelativeTo(null);
+        dialog.setBackground(new Color(30, 30, 30));
+
+        c.insets = new Insets(1, 1, 1, 1);
+        c.gridy = 0;
+        c.fill = GridBagConstraints.VERTICAL;
+
+        int counter = 0;
+        int w;
+        int h;
+
+        try {
+            w = artisticPanel.getW() > 0 ? artisticPanel.getW() : 42;
+            h = artisticPanel.getH() > 0 ? artisticPanel.getH() : 42;
+        } catch (NumberFormatException e) {
+            w = 70;
+            h = 70;
+        }
+
+        for (BufferedArtImage img : tiles) {
+            JLabel label = new JLabel(new ImageIcon());
+
+            label.setSize(new Dimension(70, 70));
+            label.setMaximumSize(new Dimension(70, 70));
+            label.setMinimumSize(new Dimension(70, 70));
+            label.setPreferredSize(new Dimension(70, 70));
+            label.setIcon(new ImageIcon(new RectangleShape(img, w, h).getThumbnail()));
+
+            if (counter == 7) {
+                counter = 0;
+                c.gridy++;
+            }
+
+            p.add(label, c);
+
+            counter++;
+        }
+
+        scrollPane.setViewportView(p);
+
+        dialog.add(scrollPane, BorderLayout.CENTER);
+
+        dialog.setVisible(true);
     }
 }
