073b65f5ff4958dbf7a58b63a93a11b70ebef285
Nils Pukropp <Nils.Pukropp@outlook.de>
2020-06-28 08:17:22 +0200

    Created abstract calculator class

---
+++ iMage/iMage.mosaique/src/main/java/org/iMage/mosaique/AbstractCalculator.java
@@ -0,0 +1,20 @@
+package org.iMage.mosaique;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Wrapper class for the {@link TriangleCalculator} and
+ * {@link RectangleCalculator} that makes both deprecated.
+ *
+ * @author Nils Purkopp
+ *
+ */
+public abstract class AbstractCalculator {
+
+    public int averageColor(BufferedImage region) {
+        // TODO: implement
+        //
+        return 0;
+    }
+
+}
